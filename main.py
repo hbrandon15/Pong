fd87220e91a3a29320833080ef82980c659d4e35
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-21 15:35:33 -0500

    Add paddle setup for Pong game

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
+
+# Paddle A
+paddle = turtle.Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+screen.update()
 screen.exitonclick() # Close the window on click
