4e1973f0a1c9a5d02eaf568985e9cd48affe10de
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-21 16:02:03 -0500

    Add Paddle class implementation for Pong game

---
+++ paddle.py
@@ -0,0 +1,18 @@
+import turtle
+
+class Paddle(turtle.Turtle):
+    def __init__(self): # define the paddle
+        super().__init__() # initialize the turtle
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(350, 0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
