0f56d186320ddc9ff8e1d4a9a540a58b100d85d9
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-21 14:57:34 -0500

    Refactor screen setup for Pong game and update title

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 import turtle
 
-
-turtle.setup(width=800, height=600)
 screen = turtle.Screen()
-screen.title("Pong Game")
+screen.setup(width=800, height=600)
+screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
-screen.mainloop() # Keep the window open
 screen.exitonclick() # Close the window on click
