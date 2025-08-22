f5e5fc3b564c81a3f0b851df0862be1f1df8dede
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-22 11:11:25 -0500

    Refactor main.py to import Paddle and Ball classes directly; add .gitignore to exclude Python cache files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
-from  turtle import Screen, Turtle
-import paddle
+from  turtle import Screen
+from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -8,20 +9,28 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-# Paddle A
-l_paddle = paddle.Paddle((-350, 0))
-r_paddle = paddle.Paddle((350, 0))
+ball = Ball()
+ball.reset_position()
 
 
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+# Keyboard controls
 screen.listen()
 screen.onkeypress(l_paddle.go_up, "w")
 screen.onkeypress(l_paddle.go_down, "s")
 screen.onkeypress(r_paddle.go_up, "Up")
 screen.onkeypress(r_paddle.go_down, "Down")
 
+
+
+
 game_is_on = True
 while game_is_on:
+	
 	screen.update()
+	ball.move()
 
 
 screen.exitonclick() # Close the window on click
