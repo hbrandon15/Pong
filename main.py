fa761e9802e5a936ff8fe8cb63eaaf0fe2af6d9b
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-22 12:38:50 -0500

    Add Scoreboard initialization and display in game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from  turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -14,6 +15,7 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 # Keyboard controls
