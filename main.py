2fc874d657f0317b2b206353437e781e0714c962
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-22 12:24:41 -0500

    Refactor miss detection logic in game loop for clarity

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ while game_is_on:
 	if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
 		ball.bounce_x()
 
+	# detect miss
+	if ball.xcor() > 380 or ball.xcor() < -380:
+		ball.reset_position()
+		
+
 
 
 screen.exitonclick() # Close the window on click
